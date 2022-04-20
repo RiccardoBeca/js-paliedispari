e0bb249992bd844c30ececb3f9e1b0e479e10ab5
RiccardoBeca <riccardobeca@gmail.com>
2022-04-20 13:38:33 +0100

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+Bonus: L’inserimento avviene tramite un campo input
+
+
+1. chiedo una parola all'utente con un prompt
+2. creo funzione 
+
+*/
+
+
+const insertWord = prompt("Please insert a word");
+
+function isPalindrome()
